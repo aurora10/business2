e2878e38c05e3d8c39fcad4438913c66ec6e779d
aurora10 <aurora10@gmail.com>
2022-04-18 19:01:09 +0200

    http to https

--- public/vendor/laravel-admin-ext/ckeditor/README.md
+++ public/vendor/laravel-admin-ext/ckeditor/README.md
@@ -36,4 +36,4 @@ To test your installation, just call the following page at your website:
 
 For example:
 
-	http://www.example.com/ckeditor/samples/index.html
+	https://www.example.com/ckeditor/samples/index.html
--- public/vendor/laravel-admin/bootstrap-fileinput/js/plugins/purify.js
+++ public/vendor/laravel-admin/bootstrap-fileinput/js/plugins/purify.js
@@ -592,7 +592,7 @@ function createDOMPurify() {
 
     /* Allow valid data-* attributes: At least one character after "-"
         (https://html.spec.whatwg.org/multipage/dom.html#embedding-custom-non-visible-data-with-the-data-*-attributes)
-        XML-compatible (https://html.spec.whatwg.org/multipage/infrastructure.html#xml-compatible and http://www.w3.org/TR/xml/#d0e804)
+        XML-compatible (https://html.spec.whatwg.org/multipage/infrastructure.html#xml-compatible and https://www.w3.org/TR/xml/#d0e804)
         We don't need to check the value; it's always URI safe. */
     if (ALLOW_DATA_ATTR && DATA_ATTR$$1.test(lcName)) {
       // This attribute is safe
